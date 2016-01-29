586c196dbc18e4201ffde3a22d20f52e76390ef4
Apparaten <bratt.anders@gmail.com>
2016-01-29 13:14:41 +0100

    Fixed missing LevellingTest

---
+++ Tests/Levellingtesting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using NUnit.Common;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Levellingtesting
+    {
+        [Test]
+        public void WizardLevelsUp()
+        {
+            var wizard = new Wizard();
+            wizard.Experience += 300d;
+            Assert.That(wizard.Level, Is.AtLeast(2));
+        }
+    }
+}
